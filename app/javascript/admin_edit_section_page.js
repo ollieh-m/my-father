d44ea8f3d1692f1c3349fe260c23e1f7de59b4ca
Ollie Haydon-Mulligan <ollie.bhm@gmail.com>
2018-11-23 19:33:26 +0000

    Finish styling buttons

--- app/javascript/admin_edit_section_page.js
+++ app/javascript/admin_edit_section_page.js
@@ -13,11 +13,11 @@ class AdminEditSectionPage {
       if (this.$add_new_version_toggle.data('closed')) {
         this.$add_new_version_toggle.text('Undo')
         this.$add_new_version_toggle.data('closed', false)
-        this.$add_new_version_toggle.before(this.add_new_version_form)
+        this.$add_new_version_toggle.parent().before(this.add_new_version_form)
       } else {
         this.$add_new_version_toggle.text('Add new version')
         this.$add_new_version_toggle.data('closed', true)
-        this.$add_new_version_toggle.prev().remove()
+        this.$add_new_version_toggle.parent().prev().remove()
       }
     })
 
