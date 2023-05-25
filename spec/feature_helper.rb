6d7c9487b70feb20f470a178f66bc5a12f0fbb57
Ollie Haydon-Mulligan <ollie.bhm@gmail.com>
2023-05-25 11:19:12 +0100

    Check with chrome headless in github actions

--- spec/feature_helper.rb
+++ spec/feature_helper.rb
@@ -4,6 +4,6 @@ require 'rails_helper'
 require 'capybara/rspec'
 require 'selenium/webdriver'
 
-Capybara.javascript_driver = :selenium_headless
+Capybara.javascript_driver = :selenium_chrome_headless
 
 Capybara.enable_aria_label = true
