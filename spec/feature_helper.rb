c921da3f852b5d49911d2f610102e728603c6f8d
Ollie Haydon-Mulligan <ollie.bhm@gmail.com>
2023-05-25 16:31:19 +0100

    Switch to headless chrome

--- spec/feature_helper.rb
+++ spec/feature_helper.rb
@@ -4,6 +4,16 @@ require 'rails_helper'
 require 'capybara/rspec'
 require 'selenium/webdriver'
 
-Capybara.javascript_driver = :selenium_headless
+Capybara.register_driver :headless_chrome do |app|
+  browser_options = ::Selenium::WebDriver::Chrome::Options.new
+  browser_options.args << '--disable-extensions'
+  browser_options.args << '--disable-gpu'
+  browser_options.args << '--no-sandbox'
+  browser_options.args << '--headless'
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
+end
+
+Capybara.javascript_driver = :headless_chrome
 
 Capybara.enable_aria_label = true
