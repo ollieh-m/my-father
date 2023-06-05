7cd2a4809389a2382471cbbdd34bae2e58797266
Ollie Haydon-Mulligan <ollie.bhm@gmail.com>
2023-06-05 18:13:53 +0100

    Add todos

--- config/initializers/database_connection_error_handling.rb
+++ config/initializers/database_connection_error_handling.rb
@@ -2,15 +2,15 @@ require "active_record/connection_adapters/postgresql_adapter"
 
 module DatabaseConnectionRetry
   def new_client(*args)
-    with_retries(attempts_remaining: 5, wait_time: 0.5) do
+    with_retries(attempts_remaining: 5, wait_time: 0.5) do # should this config come from database.yml
       super
     end
   end
 
-  def with_retries(attempts_remaining:, wait_time:, &attempt)
+  def with_retries(attempts_remaining:, wait_time:, &attempt) # make private
     yield
   rescue
-    if attempts_remaining > 0
+    if attempts_remaining > 0 # should this be > 1 to attempt the correct no. of times
       sleep(wait_time)
 
       attempts_remaining -= 1
