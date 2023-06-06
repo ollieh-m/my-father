b0715b148d74da2c4efe035ebed659180b698ee2
Ollie Haydon-Mulligan <ollie.bhm@gmail.com>
2023-06-06 14:13:31 +0100

    Refactor

--- config/initializers/database_connection_error_handling.rb
+++ config/initializers/database_connection_error_handling.rb
@@ -1,24 +1,27 @@
 require "active_record/connection_adapters/postgresql_adapter"
 
 module DatabaseConnectionRetry
-  def new_client(*args)
-    with_retries(attempts_remaining: 5, wait_time: 0.5) do # should this config come from database.yml
-      super
+  module ClassMethods
+    def new_client(*args)
+      with_retries(attempts_remaining: 5, wait_time: 0.5) do
+        super
+      end
     end
-  end
 
-  def with_retries(attempts_remaining:, wait_time:, &attempt) # make private
-    yield
-  rescue
-    if attempts_remaining > 0 # should this be > 1 to attempt the correct no. of times
-      sleep(wait_time)
+    private
 
-      attempts_remaining -= 1
-      with_retries(attempts_remaining:, wait_time:, &attempt)
-    else
-      raise
-    end
+      def with_retries(attempts_remaining:, wait_time:, &attempt)
+        attempts_remaining -= 1
+        yield
+      rescue
+        if attempts_remaining > 0
+          sleep(wait_time)
+          with_retries(attempts_remaining:, wait_time:, &attempt)
+        else
+          raise
+        end
+      end
   end
 end
 
-ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.singleton_class.prepend DatabaseConnectionRetry
+ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.singleton_class.prepend DatabaseConnectionRetry::ClassMethods
